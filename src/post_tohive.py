33b7a1e4469046535f5319c64241373a28ccfea6
Sujaymc <smc86uk@gmail.com>
2024-12-19 16:12:46 +0000

    new

--- src/post_tohive.py
+++ src/post_tohive.py
@@ -3,8 +3,7 @@ from pyspark.sql.functions import *
 
 spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432"
-                                             "/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "people").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "person").option("user", "consultants").option("password", "WelcomeItc@2022").load()
 df.printSchema()
 
 # Define the calculation of age
@@ -13,5 +12,5 @@ df_age = df.withColumn("DOB", to_date(col("DOB"), "M/d/yyyy")) \
 df_age.show(10)
 
 
-sorted_df.write.mode("overwrite").saveAsTable("sujay.people_sujay")
+sorted_df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.people_sujay")
 print("Successfully Load to Hive")
