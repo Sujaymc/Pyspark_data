662fec3db44172fd62d2ccee4a5fcb256393a794
Sujaymc <smc86uk@gmail.com>
2024-12-20 11:54:10 +0000

    update

--- src/incre_post_hive.py
+++ src/incre_post_hive.py
@@ -8,13 +8,7 @@ str(m_id)
 
 query = 'SELECT * FROM sujay_sales WHERE "sale_id" > ' + str(m_id)
 
-more_data = spark.read.format("jdbc") \
-    .option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb") \
-    .option("driver", "org.postgresql.Driver") \
-    .option("user", "consultants") \
-    .option("password", "WelcomeItc@2022") \
-    .option("query", query) \
-    .load()
+more_data = spark.read.format("jdbc").option("url", "jdbc:postgresql://ec2-3-9-191-104.eu-west-2.compute.amazonaws.com:5432/testdb").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").option("query", query).load()
 
 df_increment.write.mode("append").saveAsTable("bigdata_nov_2024.sujay_sales")
 print("Successfully Load to Hive")
