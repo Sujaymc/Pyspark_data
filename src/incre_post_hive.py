d5f3cf40bfdef32d7f2584f24bedc6e1b1b1a883
Sujaymc <smc86uk@gmail.com>
2024-12-20 16:07:13 +0000

    with try

--- src/incre_post_hive.py
+++ src/incre_post_hive.py
@@ -1,14 +1,28 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
-max_id = spark.sql("SELECT max(sale_id) FROM bigdata_nov_2024.sujay_sales")
-m_id = max_id.collect()[0][0]
-str(m_id)
+try:
+    # Initialize SparkSession with Hive support
+    spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
-query = 'SELECT * FROM sujay_sales WHERE sale_id > ' + str(m_id)
+    # Fetch the maximum sale_id from Hive table
+    max_id = spark.sql("SELECT max(sale_id) FROM bigdata_nov_2024.sujay_sales")
+    m_id = max_id.collect()[0][0]
 
-more_data = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").option("query", query).load()
+    # Ensure m_id is not None before proceeding
+    if m_id is None:
+        m_id = 0
 
-more_data.write.mode("append").saveAsTable("bigdata_nov_2024.sujay_sales")
-print("Successfully Load to Hive")
+    # Formulate the query to fetch new data from PostgreSQL
+    query = 'SELECT * FROM sujay_sales WHERE sale_id > ' + str(m_id)
+
+    # Read data from PostgreSQL using the query
+    more_data = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").option("query", query).load()
+
+    # Append the new data to Hive table
+    more_data.write.mode("append").saveAsTable("bigdata_nov_2024.sujay_sales")
+    print("Successfully Load to Hive")
+
+except Exception as e:
+    # Print the error message
+    print("Error occurred:", str(e))
--- src/post_tohive.py
+++ src/post_tohive.py
@@ -1,11 +1,21 @@
 from pyspark.sql import *
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
-df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "sujay_sales").option("user", "consultants").option("password", "WelcomeItc@2022").load()
-df.printSchema()
+try:
+    # Initialize SparkSession with Hive support
+    spark = SparkSession.builder.master("local").appName("MiniProj").enableHiveSupport().getOrCreate()
 
+    # Read data from PostgreSQL
+    df = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("dbtable", "sujay_sales").option("user", "consultants").option("password", "WelcomeItc@2022").load()
+    
+    # Print the schema of the DataFrame
+    df.printSchema()
 
-df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.sujay_sales")
-print("Successfully Load to Hive")
+    # Write the DataFrame to Hive
+    df.write.mode("overwrite").saveAsTable("bigdata_nov_2024.sujay_sales")
+    print("Successfully Load to Hive")
+
+except Exception as e:
+    # Print the error message
+    print("Error occurred:", str(e))
