edc77e664043848b8169c852d91490425c06706f
Sujaymc <smc86uk@gmail.com>
2024-12-20 12:09:28 +0000

    da

--- src/incre_post_hive.py
+++ src/incre_post_hive.py
@@ -6,7 +6,7 @@ max_id = spark.sql("SELECT max(sale_id) FROM bigdata_nov_2024.sujay_sales")
 m_id = max_id.collect()[0][0]
 str(m_id)
 
-query = 'SELECT * FROM bigdata_nov_2024.sujay_sales WHERE "sale_id" > ' + str(m_id)
+query = 'SELECT * FROM bigdata_nov_2024.sujay_sales WHERE sale_id > ' + str(m_id)
 
 more_data = spark.read.format("jdbc").option("url", "jdbc:postgresql://18.132.73.146:5432/testdb").option("driver", "org.postgresql.Driver").option("user", "consultants").option("password", "WelcomeItc@2022").option("query", query).load()
 
